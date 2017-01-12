6d76a951cecaa92de14e15a367041cd88971db2c
Matthew <matthewwebb65@yahoo.com>
2017-01-12 11:56:26 -0600

    added framework for popup & proper communication between content and bkrd

--- background.js
+++ background.js
@@ -1,22 +1,25 @@
 var currDomainIdx = null;
 var domains = [];
 var times = [];
-var locked = false;
 
 var update = function() {
-	locked = true;
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.sendMessage(tabs[0].id, {message: "act"}, function(response) {
-			if (domains.indexOf(response.domain) < 0) {
-				domains.push(response.domain);
-				times.push(0);
+			if (response == null) {
+				currDomainIdx = null;
+			} else {
+				if (domains.indexOf(response) < 0) {
+					domains.push(response);
+					times.push(0);
+				}
+				
+				currDomainIdx = domains.indexOf(response);
+				for (i = 0; i < domains.length; i++) {
+					console.log(domains[i] + ": " + (times[i] / 1000.0) + " seconds");
+				}
 			}
-			
-			currDomainIdx = domains.indexOf(response.domain);
-			document.getElementById('smname')[0].value = 100;
 		});
 	});
-	locked = false;
 };
 
 chrome.tabs.onActivated.addListener(function(activeInfo) {
@@ -29,9 +32,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 
 var lastTime = Date.now();
 setInterval(function() {
-	if (!locked) {
-		var currTime = Date.now();
-		times[currDomainIdx] += currTime - lastTime;
-		lastTime = currTime;
-	}
+	var currTime = Date.now();
+	times[currDomainIdx] += currTime - lastTime;
+	lastTime = currTime;
 }, 0);
--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	if (request.message == "act") {
-		sendResponse({domain: document.domain});
+		sendResponse(document.domain);
 	}
 });
